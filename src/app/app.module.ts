99696a7282ff68a43fea188a9b2b77852425b64b
Derek Lin <dereklin592@gmail.com>
2020-06-22 21:43:12 +0800

    1. Install Angualr Material package.
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/app.module.ts
    	modified:   src/index.html
    	modified:   src/main.ts
    	modified:   src/styles.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorModule } from './calculator/calculator.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CalculatorModule } from './calculator/calculator.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CalculatorModule
+    CalculatorModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
