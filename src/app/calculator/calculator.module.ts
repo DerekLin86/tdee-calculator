5919b074e3b907b0b79a52b72dc2eca92f4d0f74
Derek Lin <derek.lin@careline.com.tw>
2020-06-27 23:54:16 +0800

    1. Changing the style of td-input-formcontrol.
    	modified:   src/app/calculator/calculator.component.html
    	modified:   src/app/calculator/calculator.module.ts
    	modified:   src/app/calculator/scss/mobile.scss
    	modified:   src/app/shared/tools/td-input-formcontrol/scss/mobile.scss
    	modified:   src/app/shared/tools/td-input-formcontrol/td-input-formcontrol.component.html
    	modified:   src/app/shared/tools/td-input-formcontrol/td-input-formcontrol.module.ts

--- src/app/calculator/calculator.module.ts
+++ src/app/calculator/calculator.module.ts
@@ -6,12 +6,14 @@ import { MatRadioModule } from '@angular/material/radio';
 import { CalculatorComponent } from './calculator.component';
 import { TdInputFormcontrolModule  } from '../shared/tools/td-input-formcontrol/td-input-formcontrol.module';
 import { TdDropdownModule } from '../shared/tools/td-dropdown/td-dropdown.module';
+import { TdButtonModule } from '../shared/tools/td-button/td-button.module';
 
 
 @NgModule({
   declarations: [CalculatorComponent],
   imports: [
     CommonModule,
+    TdButtonModule,
     MatInputModule,
     MatRadioModule,
     TdDropdownModule,
--- src/app/shared/tools/td-input-formcontrol/td-input-formcontrol.module.ts
+++ src/app/shared/tools/td-input-formcontrol/td-input-formcontrol.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TdInputFormcontrolComponent } from './td-input-formcontrol.component';
 
+import {MatInputModule} from '@angular/material/input';
+
 
 
 @NgModule({
   declarations: [TdInputFormcontrolComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule
   ],
   exports: [
     TdInputFormcontrolComponent
