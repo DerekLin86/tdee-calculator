49d55051a338c50f9f66e5ba490cd9ce9dfb7112
Derek Lin <derek.lin@careline.com.tw>
2020-06-29 00:18:49 +0800

    1. Implement the display logic of basic block.
    	modified:   src/app/calculator/calculator.component.html
    	modified:   src/app/calculator/calculator.component.ts
    	modified:   src/app/calculator/scss/mobile.scss

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
 import { TdDrowdown } from '../shared/tools/td-dropdown/td-dropdown';
+import { TdRadioButtonGroupComponent } from '../shared/tools/td-radio-button-group/td-radio-button-group.component';
 import { TdRadioButtonGroup } from '../shared/tools/td-radio-button-group/td-radio-button-group';
 
 @Component({
@@ -8,22 +9,26 @@ import { TdRadioButtonGroup } from '../shared/tools/td-radio-button-group/td-rad
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss']
 })
-export class CalculatorComponent implements OnInit {
+export class CalculatorComponent implements AfterViewInit, OnInit {
 
   public viewModel = {
     bmrRadioOptions: [{
       text: '知道',
-      value: '1'
+      value: '1',
+      selected: false
     }, {
       text: '不知道',
-      value: '0'
+      value: '0',
+      selected: true
     }] as Array<TdRadioButtonGroup.Option>,
     sexRadioOptions: [{
       text: '男',
-      value: '1'
+      value: '1',
+      selected: true
     }, {
       text: '女',
-      value: '2'
+      value: '2',
+      selected: false
     }] as Array<TdRadioButtonGroup.Option>,
     feqDropdownArg: {
       title: '每週運動頻率'
@@ -33,12 +38,33 @@ export class CalculatorComponent implements OnInit {
     } as TdDrowdown.Argument,
     extraCalDropdownArg: {
       title: '額外卡路里'
-    }
+    },
+    isUnknown: null as boolean
   };
+
+  @ViewChild('knowBmr', {static: true})
+  knowBmrRadioComponet: TdRadioButtonGroupComponent;
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.initFormValue();
+  }
+
+  bmrOptionChange(value: string | number) {
+    this.viewModel.isUnknown =
+      this.viewModel.bmrRadioOptions.find(
+        option => option.text === '不知道'
+      ).value === value;
+  }
+
+  private initFormValue() {
+    this.viewModel.isUnknown = this.viewModel.bmrRadioOptions.find(
+      option => option.text === '不知道'
+    ).selected;
+  }
 }
