1af7135025103f64bff0a80dc62061a1f4cd5bfe
Derek Lin <derek.lin@careline.com.tw>
2020-07-22 00:52:52 +0800

    1. Disable the next button while the value of input-formcontrol    is null. 2. Clean the value of input-formcontrol while clicking unknown    button.
    	modified:   src/app/bmr-asker/bmr-asker.component.html
    	modified:   src/app/bmr-asker/bmr-asker.component.ts

--- src/app/bmr-asker/bmr-asker.component.ts
+++ src/app/bmr-asker/bmr-asker.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { TdInputFormcontrolComponent } from '../shared/tools/td-input-formcontrol/td-input-formcontrol.component';
 
 @Component({
   selector: 'app-bmr-asker',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BmrAskerComponent implements OnInit {
 
+  @ViewChild(TdInputFormcontrolComponent, {static: true})
+  tdInputFormcontrolComponent: TdInputFormcontrolComponent;
+
+  get disableNextButton() {
+    return this.tdInputFormcontrolComponent &&
+      this.tdInputFormcontrolComponent.viewModel.value.toString().length <= 0;
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  clearInputData() {
+    if (this.tdInputFormcontrolComponent) {
+      this.tdInputFormcontrolComponent.viewModel.value = '';
+    }
   }
 
 }
