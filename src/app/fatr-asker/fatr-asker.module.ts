b0bacb5453ac4da0cdee01db8613d122b2744415
Derek Lin <derek.lin@careline.com.tw>
2020-08-17 01:15:34 +0800

    1. Implement FATR asker page.
    	modified:   src/app/calculator/calculator.component.html
    	modified:   src/app/fatr-asker/fatr-asker.component.html
    	modified:   src/app/fatr-asker/fatr-asker.component.scss
    	modified:   src/app/fatr-asker/fatr-asker.module.ts
    	new file:   src/app/fatr-asker/scss/mobile.scss

--- src/app/fatr-asker/fatr-asker.module.ts
+++ src/app/fatr-asker/fatr-asker.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FatrAskerComponent } from './fatr-asker.component';
 
+import { FatrAskerComponent } from './fatr-asker.component';
+import { TdButtonModule } from '../shared/tools/td-button/td-button.module';
 
 
 @NgModule({
   declarations: [FatrAskerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TdButtonModule
   ],
   exports: [
     FatrAskerComponent
