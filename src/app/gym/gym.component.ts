40c139b26e573f2f44103fc01d9431bd24b0c40d
Derek Lin <derek.lin@careline.com.tw>
2020-08-26 21:37:16 +0800

    1. Add gym list.
    	modified:   src/app/gym/gym.component.html
    	modified:   src/app/gym/gym.component.ts
    	modified:   src/app/gym/gym.module.ts
    	modified:   src/app/gym/scss/mobile.scss

--- src/app/gym/gym.component.ts
+++ src/app/gym/gym.component.ts
@@ -1,11 +1,27 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
+import { TdRadioButtonGroup } from '../shared/tools/td-radio-button-group/td-radio-button-group';
+
 @Component({
   selector: 'app-gym',
   templateUrl: './gym.component.html',
   styleUrls: ['./gym.component.scss']
 })
 export class GymComponent implements OnInit {
+
+  public radioOptions: Array<TdRadioButtonGroup.Option> = [{
+    text: 'Word Gym',
+    value: 'Word Gym'
+  }, {
+    text: '健身工廠',
+    value: '健身工廠'
+  }, {
+    text: '成吉思汗',
+    value: '成吉思汗'
+  }, {
+    text: '享健身',
+    value: '享健身'
+  }];
 
   @Input()
   submitCallback: () => void;
--- src/app/gym/gym.module.ts
+++ src/app/gym/gym.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GymComponent } from './gym.component';
 import { TdButtonModule } from '../shared/tools/td-button/td-button.module';
+import { TdRadioButtonGroupModule } from '../shared/tools/td-radio-button-group/td-radio-button-group.module';
+import { TdInputFormcontrolModule } from '../shared/tools/td-input-formcontrol/td-input-formcontrol.module';
 
 
 
@@ -9,7 +11,9 @@ import { TdButtonModule } from '../shared/tools/td-button/td-button.module';
   declarations: [GymComponent],
   imports: [
     CommonModule,
-    TdButtonModule
+    TdButtonModule,
+    TdInputFormcontrolModule,
+    TdRadioButtonGroupModule
   ],
   exports: [
     GymComponent
