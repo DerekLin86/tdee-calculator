7a1f7a40b63b495e1c244ce5c87e166f1f0eb3c4
Derek Lin <derek.lin@careline.com.tw>
2020-08-27 20:59:04 +0800

    1. Add the images of fatr. 2. Display all options.
    	modified:   src/app/fatr-asker/fatr-asker.component.html
    	modified:   src/app/fatr-asker/fatr-asker.component.ts
    	modified:   src/app/fatr-asker/scss/mobile.scss
    	new file:   src/assets/images/fatr-asker/female1.png
    	new file:   src/assets/images/fatr-asker/female2.png
    	new file:   src/assets/images/fatr-asker/female3.png
    	new file:   src/assets/images/fatr-asker/female4.png
    	new file:   src/assets/images/fatr-asker/female5.png
    	new file:   src/assets/images/fatr-asker/female6.png
    	new file:   src/assets/images/fatr-asker/male1.png
    	new file:   src/assets/images/fatr-asker/male2.png
    	new file:   src/assets/images/fatr-asker/male3.png
    	new file:   src/assets/images/fatr-asker/male4.png
    	new file:   src/assets/images/fatr-asker/male5.png
    	new file:   src/assets/images/fatr-asker/male6.png

--- src/app/fatr-asker/fatr-asker.component.ts
+++ src/app/fatr-asker/fatr-asker.component.ts
@@ -7,6 +7,47 @@ import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 })
 export class FatrAskerComponent implements OnInit {
 
+  public viewModel = {
+    femaleOptions: [{
+      imageUrl: 'assets/images/fatr-asker/female1.png',
+      text: '< 10%'
+    }, {
+      imageUrl: 'assets/images/fatr-asker/female2.png',
+      text: '10% ~ 15%'
+    }, {
+      imageUrl: 'assets/images/fatr-asker/female3.png',
+      text: '15% ~ 20%'
+    }, {
+      imageUrl: 'assets/images/fatr-asker/female4.png',
+      text: '20% ~ 25%'
+    }, {
+      imageUrl: 'assets/images/fatr-asker/female5.png',
+      text: '25% ~ 30%'
+    }, {
+      imageUrl: 'assets/images/fatr-asker/female6.png',
+      text: '> 30%'
+    }],
+    maleOptions: [{
+      imageUrl: 'assets/images/fatr-asker/male1.png',
+      text: '< 10%'
+    }, {
+      imageUrl: 'assets/images/fatr-asker/male2.png',
+      text: '10% ~ 15%'
+    }, {
+      imageUrl: 'assets/images/fatr-asker/male3.png',
+      text: '15% ~ 20%'
+    }, {
+      imageUrl: 'assets/images/fatr-asker/male4.png',
+      text: '25% ~ 30%'
+    }, {
+      imageUrl: 'assets/images/fatr-asker/male5.png',
+      text: '25% ~ 30%'
+    }, {
+      imageUrl: 'assets/images/fatr-asker/male6.png',
+      text: '> 30%'
+    }]
+  };
+
   @Input()
   submitCallback: () => void;
 
