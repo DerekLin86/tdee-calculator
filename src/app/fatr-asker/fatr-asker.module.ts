f6f8ecfc8447ce8de5795131b346a3f982776c10
Derek Lin <derek.lin@careline.com.tw>
2020-08-31 23:31:20 +0800

    1. Apply static path.
    	modified:   src/app/fatr-asker/fatr-asker.component.html
    	modified:   src/app/fatr-asker/fatr-asker.module.ts
    	modified:   src/app/static-path/static-path.pipe.ts

--- src/app/fatr-asker/fatr-asker.module.ts
+++ src/app/fatr-asker/fatr-asker.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { FatrAskerComponent } from './fatr-asker.component';
 import { TdButtonModule } from '../shared/tools/td-button/td-button.module';
+import { StaticPathModule } from '../static-path/static-path.module';
 
 
 @NgModule({
   declarations: [FatrAskerComponent],
   imports: [
     CommonModule,
+    StaticPathModule,
     TdButtonModule
   ],
   exports: [
--- src/app/static-path/static-path.pipe.ts
+++ src/app/static-path/static-path.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+declare var getSfStaticResourceRoot: () => string;
+
+export function getStaticPathForResource(resourcePath : string) {
+    return getSfStaticResourceRoot() + resourcePath;
+}
+
 @Pipe({
   name: 'staticPath'
 })
 export class StaticPathPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: string): string {
+    return getStaticPathForResource(value);
   }
 
 }
